7ae4248af9d482c15d05fddcc4c59b705ee81f1b
Charlie Jackson <charlie@charliejackson.com>
2025-05-25 19:15:34 +0100

    chore(2048): 🔧 remove the temp logic
    branch main

--- game/games/two048.ts
+++ game/games/two048.ts
@@ -72,59 +72,8 @@ const getInitState: Types.GetInitState = ({ rand, gridSize }) => {
 };
 
 const applyMove: Types.ApplyMove = ({ state, direction, gridSize, rand }) => {
-  let nextState: Types.GameState | null = {
-    tiles: state.tiles.map((tile): Types.Tile => {
-      switch (direction) {
-        case "up":
-          return {
-            ...tile,
-            position: [Math.max(tile.position[0] - 1, 0), tile.position[1]],
-          };
-        case "down":
-          return {
-            ...tile,
-            position: [
-              Math.min(tile.position[0] + 1, gridSize.rows - 1),
-              tile.position[1],
-            ],
-          };
-        case "left":
-          return {
-            ...tile,
-            position: [tile.position[0], Math.max(tile.position[1] - 1, 0)],
-          };
-        case "right":
-          return {
-            ...tile,
-            position: [
-              tile.position[0],
-              Math.min(tile.position[1] + 1, gridSize.columns - 1),
-            ],
-          };
-      }
-    }),
-    score: 0,
-    state: "playing",
-  };
-
-  nextState = spawnTile({
-    gridSize,
-    rand,
-    state: nextState,
-    tile: {
-      value: 2,
-      ...getColorsFromValue(2),
-    },
-  });
-
-  if (!nextState) {
-    return {
-      ...state,
-      state: "lost",
-    };
-  }
-
-  return nextState;
+  // TODO: Fill me in
+  return state;
 };
 
 const gameConfig: Types.GameConfig = {
